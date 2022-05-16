86591571042f942edbe9db80a8df08966a98ed0b
Matthias Lischka <matthiaslischka@users.noreply.github.com>
2022-05-16 09:11:14 +0200

    Batman commit

---
+++ AutoMapperNullEnumIssue/UnitTest1.cs
@@ -0,0 +1,58 @@
+using AutoMapper;
+using AutoMapper.Extensions.EnumMapping;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoMapperNullEnumIssue;
+
+public class SomeSourceDto
+{
+    public SomeSourceEnum? SomeEnum { get; set; }
+}
+
+public class SomeDestinationDto
+{
+    public SomeDestinationEnum? SomeEnum { get; set; }
+}
+
+public enum SomeSourceEnum
+{
+    First = 1
+}
+
+public enum SomeDestinationEnum
+{
+    First = 1
+}
+
+public class SomeProfile : Profile
+{
+    public SomeProfile()
+    {
+        CreateMap<SomeSourceDto, SomeDestinationDto>();
+        CreateMap<SomeSourceEnum, SomeDestinationEnum>().ConvertUsingEnumMapping(opt => opt.MapByName());
+    }
+}
+
+[TestClass]
+public class UnitTest1
+{
+    private readonly IMapper _mapper;
+
+    public UnitTest1()
+    {
+        var mapperConfiguration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(typeof(SomeProfile));
+            cfg.EnableEnumMappingValidation();
+        });
+        _mapper = mapperConfiguration.CreateMapper();
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var someSourceDto = new SomeSourceDto { SomeEnum = 0 };
+        var someDestinationDto = _mapper.Map<SomeDestinationDto>(someSourceDto);
+        Assert.IsNull(someDestinationDto.SomeEnum);
+    }
+}
